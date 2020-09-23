9a3af0f61e5b5b0916dc8fb053c40005ae65f97b
Arthur Barbosa <arthur.barbosa@itbam.org.br>
2020-09-23 10:00:17 -0400

    [added]configured to receive data in json

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,4 +2,8 @@
 
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Servidor rodando tudo certo'))
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Metodo post')
+    })
 }
